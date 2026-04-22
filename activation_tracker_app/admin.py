d80ba9b194e95e7d21bcdd9061758e32f1a3bf76
barioder <barioder27@gmail.com>
2026-04-22 22:21:27 +0300

    registered model applications

--- activation_tracker_app/admin.py
+++ activation_tracker_app/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
 # Register your models here.
+from models.merchant import Merchant
+from models.requirement import Requirement
+
+admin.site.register(Merchant)
+admin.site.register(Requirement)
